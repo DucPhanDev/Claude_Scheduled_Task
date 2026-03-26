2f5bc88c4b6620827d9e8a23d24f9fb3ebee68cd
Đức.Phan Lê Công <ducplc@vng.com.vn>
2026-03-26 11:59:58 +0700

    -Add DoTween

--- SampleScheduledTask/Assets/Scripts/System/Events/IEvent.cs
+++ SampleScheduledTask/Assets/Scripts/System/Events/IEvent.cs
@@ -4,39 +4,3 @@ public interface IEvent
 {
 
 }
-
-public class OnDirectionSelectedEvent : IEvent
-{
-    public Vector2 direction;  // vector hướng đã chọn (normalized)
-}
-
-
-public class GameManagerReadyEvent : IEvent
-{
-
-}
-
-public class DrinkFinishedCheckEvent : IEvent
-{
-
-}
-
-public class DrinkSpawnedEvent : IEvent
-{
-    public int currentDrinkId;
-    public int nextDrinkId;
-}
-
-public class DrinkDeliveredEvent : IEvent
-{
-    public int money;
-}
-public class TimeUpdatedEvent : IEvent
-{
-    public float remainingTime;
-}
-public class DrinkStoppedEvent : IEvent
-{
-    public Vector3 stopPosition;
-    public DrinkController drink;  // để lấy config.id, bounceCount...
-}
